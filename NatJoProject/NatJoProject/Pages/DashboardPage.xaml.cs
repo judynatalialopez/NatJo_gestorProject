ac0befbbc176e9a6b900b7e5dfeab0c8c81d085a
Johanbeltranramirez <johanframirez13@gmail.com>
2025-05-17 18:11:32 -0500

    Incorporo cambios de f-views a f-models

--- NatJoProject/NatJoProject/Pages/DashboardPage.xaml.cs
+++ NatJoProject/NatJoProject/Pages/DashboardPage.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Text;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -9,21 +13,16 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace NatJoProject
+namespace NatJoProject.Pages
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Lógica de interacción para DashboardPage.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class DashboardPage : Page
     {
-        public MainWindow()
+        public DashboardPage()
         {
             InitializeComponent();
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-
         }
     }
 }
---
+++ NatJoProject/NatJoProject/Views/BacklogView.xaml.cs
@@ -0,0 +1,38 @@
+﻿using NatJoProject.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace NatJoProject.Views
+{
+    /// <summary>
+    /// Lógica de interacción para BacklogView.xaml
+    /// </summary>
+    public partial class BacklogView : Window
+    {
+        public BacklogView()
+        {
+            InitializeComponent();
+
+            private void Dashboard_Click(object sender, RoutedEventArgs e)
+            {
+                MainFrame.Navigate(new DashboardPage());
+            }
+
+            private void Backlog_Click(object sender, RoutedEventArgs e)
+            {
+                MainFrame.Navigate(new BacklogPage());
+            }
+        }
+    }
+}
